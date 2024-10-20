d10323054ce886c97cce239cc8da7fe8ec5c458e
Vibhaypandey <90547954+Vibhaypandey@users.noreply.github.com>
2024-10-20 19:41:30 +0530

    Update Client.java
    rename class name

--- LLD/EASY/TaskManagement/Client.java
+++ LLD/EASY/TaskManagement/Client.java
@@ -14,7 +14,7 @@ public class Client {
 
     public static void main(String[] args) {
 
-      TakManager takManager = TakManager.getinstance();
+      TaskManager taskManager = TaskManager.getinstance();
         // Create USERs
         USER user1 = new USER("1", "John Doe", "john@example.com");
         USER user2 = new USER("2", "Jane Smith", "jane@example.com");
@@ -23,41 +23,41 @@ public class Client {
         Task task1 = new Task("1", "Task 1", "Description 1",1, new Date(), user1);
         Task task2 = new Task("2", "Task 2", "Description 2",2, new Date(),  user2);
         Task task3 = new Task("3", "Task 3", "Description 3",1, new Date(), user1);
-        takManager.createTask(task1);
-        takManager.createTask(task2);
-        takManager.createTask(task3);
+        taskManager.createTask(task1);
+        taskManager.createTask(task2);
+        taskManager.createTask(task3);
         //update task
         task1.setTaskStatus(TaskStatus.COMPLETED);
-        takManager.updateTask(task1);
-        takManager.unassignTaskFromUser(user1,task1);
+        taskManager.updateTask(task1);
+        taskManager.unassignTaskFromUser(user1,task1);
 
 
       // Search tasks
-      List<Task> searchResults = takManager.searchTasks("Task");
+      List<Task> searchResults = taskManager.searchTasks("Task");
       System.out.println("Search Results:");
       for (Task task : searchResults) {
         System.out.println(task.getTitle());
       }
 
       // Filter tasks
-      List<Task> filteredTasks = takManager.filtertaskbased(TaskStatus.PENDING, new Date(0), new Date(), 1);
+      List<Task> filteredTasks = taskManager.filtertaskbased(TaskStatus.PENDING, new Date(0), new Date(), 1);
       System.out.println("Filtered Tasks:");
       for (Task task : filteredTasks) {
         System.out.println(task.getTitle());
       }
 
       // Mark a task as completed
-      takManager.markTaskAsCompleted("1");
+      taskManager.markTaskAsCompleted("1");
 
       // Get task history for a user
-      List<Task> taskHistory = takManager.getTaskHistory(user1);
+      List<Task> taskHistory = taskManager.getTaskHistory(user1);
       System.out.println("Task History for " + user1.getName() + ":");
       for (Task task : taskHistory) {
         System.out.println(task.getTitle());
       }
 
       // Delete a task
-      takManager.deleteTask("3");
+      taskManager.deleteTask("3");
 
 
     }
